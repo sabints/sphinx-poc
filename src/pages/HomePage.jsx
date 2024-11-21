273120e4a953f106a0e164c666d63f1e9006a4cb
sanu simon <saanusimon91@gmail.com>
2024-11-21 20:21:31 +0530

    Home page changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,163 @@
 import React from 'react';
+import './pages.scss'
 
 function HomePage() {
-  return <h1>Welcome to the Home Page</h1>;
+  return  (
+    <>
+      <section className='banner'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-sm-6 col-xs-12'>
+              <h1 className='banner_title'>
+              Delivering Enhanced 
+              Audit Quality and Insights 
+              <span className='bold_'>Driven by Technology</span>
+              </h1>
+            </div>
+            <div className='col-sm-6 col-xs-12'>
+              <div className='banner_txt'>
+                <span className='head_'>Powering Audit Efficiency, Accuracy, and Insight through Technology</span>
+                <div className='txt_style'>
+                  <p>At KPMG we believe that it is not just about reaching the right opinion, but how we reach that opinion. We consider risks to the quality of our audit in our engagement risk assessment and planning discussions.​</p>
+                </div>
+                <div className='btn_wrap'>
+                  <a href='#' className='btn_style'>View the Current Engagement</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='strategy_value_sec'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-sm-4 lft_img'>
+              <img src={import.meta.env.BASE_URL + "/src/assets/images/strategy-img.jpg"} alt="" width="auto" />
+            </div>
+            <div className='col-sm-8'>
+              <div className='txt_box'>
+              <span className='head_'>KPMG Leverages Technology to Drive Audit Strategy and Value</span>
+                <div className='txt_style'>
+                  <p>We have sought to embed technology in our audit since we became Barclays auditors in 2017, doing this thoughtfully in the areas that make sense, and only where technology delivers enhanced audit quality, increased efficiencies, and greater insight. The roadmap below demonstrates the technology we have progressively deployed year on year from 2017 – 2023, and continues to be in use today.​</p>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='engagmnt_team section_'>
+        <div className='container'>
+          <label className='title_'>Your Engagement Team</label>
+          <div className='row'>
+            <div className='col-sm-6'>
+              <div className='team_detail'>
+                <div className='img_'>
+                  <img src={import.meta.env.BASE_URL + "/src/assets/images/team-img1.png"} alt="" width="auto" />
+                </div>
+                <div className='detail_box'>
+                  <div className='name_design'>
+                    <label className='name_'>Elizabeth Neilsen</label>
+                    <span className='desig_'>Senior Partner</span>
+                  </div>
+                  <div className='contact_detail'>
+                    <span className='ph_mail'>Tel: +44 (0)20 7311 1000</span>
+                    <span className='ph_mail'>e.neilsen@kpmg.uk.co</span>
+                  </div>
+                  <div className='addrs_'>
+                    <p>London - Canada Square
+                      15 Canada Square,
+                      Canary Wharf
+                      London, E14 5GL</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='col-sm-6 right_sec'>
+              <div className='row'>
+                <div className='col-sm-6'>
+                  <div className='team_detail'>
+                    <div className='img_'>
+                      <img src={import.meta.env.BASE_URL + "/src/assets/images/team-img1.png"} alt="" width="auto" />
+                    </div>
+                    <div className='detail_box'>
+                      <div className='name_design'>
+                        <label className='name_'>Elizabeth Neilsen</label>
+                        <span className='desig_'>Senior Partner</span>
+                      </div>
+                      <div className='contact_detail'>
+                        <span className='ph_mail'>Tel: +44 (0)20 7311 1000</span>
+                        <span className='ph_mail'>e.neilsen@kpmg.uk.co</span>
+                      </div>
+                      
+                    </div>
+                  </div>
+                </div>
+                <div className='col-sm-6'>
+                <div className='team_detail'>
+                    <div className='img_'>
+                      <img src={import.meta.env.BASE_URL + "/src/assets/images/team-img1.png"} alt="" width="auto" />
+                    </div>
+                    <div className='detail_box'>
+                      <div className='name_design'>
+                        <label className='name_'>Elizabeth Neilsen</label>
+                        <span className='desig_'>Senior Partner</span>
+                      </div>
+                      <div className='contact_detail'>
+                        <span className='ph_mail'>Tel: +44 (0)20 7311 1000</span>
+                        <span className='ph_mail'>e.neilsen@kpmg.uk.co</span>
+                      </div>
+                      
+                    </div>
+                  </div>
+                </div>
+                <div className='col-sm-6'>
+                <div className='team_detail'>
+                    <div className='img_'>
+                      <img src={import.meta.env.BASE_URL + "/src/assets/images/team-img1.png"} alt="" width="auto" />
+                    </div>
+                    <div className='detail_box'>
+                      <div className='name_design'>
+                        <label className='name_'>Elizabeth Neilsen</label>
+                        <span className='desig_'>Senior Partner</span>
+                      </div>
+                      <div className='contact_detail'>
+                        <span className='ph_mail'>Tel: +44 (0)20 7311 1000</span>
+                        <span className='ph_mail'>e.neilsen@kpmg.uk.co</span>
+                      </div>
+                      
+                    </div>
+                  </div>
+                </div>
+                <div className='col-sm-6'>
+                <div className='team_detail'>
+                    <div className='img_'>
+                      <img src={import.meta.env.BASE_URL + "/src/assets/images/team-img1.png"} alt="" width="auto" />
+                    </div>
+                    <div className='detail_box'>
+                      <div className='name_design'>
+                        <label className='name_'>Elizabeth Neilsen</label>
+                        <span className='desig_'>Senior Partner</span>
+                      </div>
+                      <div className='contact_detail'>
+                        <span className='ph_mail'>Tel: +44 (0)20 7311 1000</span>
+                        <span className='ph_mail'>e.neilsen@kpmg.uk.co</span>
+                      </div>
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+    </>
+
+    )
 }
 
 export default HomePage;
