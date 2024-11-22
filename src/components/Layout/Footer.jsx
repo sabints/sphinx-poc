24cf3afa75dba5a48704d765b465a98630f24139
sanu simon <saanusimon91@gmail.com>
2024-11-22 11:26:51 +0530

    Home styles

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+        <div className='container'>
+            <ul className='foot_list'>
+                <li><a href='#'>Legal</a></li>
+                <li><a href='#'>Privacy</a></li>
+                <li><a href='#'>Accessibility</a></li>
+            </ul>
+            <p>
+            &#169; 2024 KPMG LLP a UK limited liability partnership and a member firm of the KPMG global organisation of independent member firms affiliated with KPMG International Limited, a private English company limited by guarantee. All rights reserved. <br />
+            For more detail about the structure of the KPMG global organisation please visit <a href='#'>  https://kpmg.com/governance.</a>
+            </p>
+        </div>
+    </footer>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -47,6 +47,52 @@ function HomePage() {
         </div>
       </section>
 
+      <section className='audit_timeline_ section_'>
+        <div className='container'>
+          <div className='title_bar'>
+            <label className='title_'>Automating the Audit Since 2017</label>
+            <div className='filter_options'>
+              <div className='select_box'>
+                <select className='select_'>
+                  <option>All Capability Areas</option>
+                  <option>All Capability Areas</option>
+                  <option>All Capability Areas</option>
+                  <option>All Capability Areas</option>
+                </select>
+              </div>
+              <div className='select_box'>
+                <select className='select_'>
+                  <option>All Business Units</option>
+                  <option>All Business Units</option>
+                  <option>All Business Units</option>
+                  <option>All Business Units</option>
+                </select>
+              </div>
+              <div className='select_box'>
+                <select className='select_'>
+                  <option>All FS Captions</option>
+                  <option>All FS Captions</option>
+                  <option>All FS Captions</option>
+                  <option>All FS Captions</option>
+                </select>
+              </div>
+              <div className='select_box'>
+                <select className='select_'>
+                  <option>All Years Deployed</option>
+                  <option>All Years Deployed</option>
+                  <option>All Years Deployed</option>
+                  <option>All Years Deployed</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className='timeline_sec'>
+            <img src={import.meta.env.BASE_URL + "/src/assets/images/headers.png"} alt="" width="auto" />
+            <img src={import.meta.env.BASE_URL + "/src/assets/images/table.png"} alt="" width="auto" />
+          </div>
+        </div>
+      </section>
+
       <section className='engagmnt_team section_'>
         <div className='container'>
           <label className='title_'>Your Engagement Team</label>
@@ -150,6 +196,14 @@ function HomePage() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='crent_engmnt section_'>
+        <div className='container'>
+          <div className='btn_wrap'>
+            <a href='#' className='btn_style'>View the Current Engagement</a>
           </div>
         </div>
       </section>
